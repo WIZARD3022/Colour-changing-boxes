089bd0c72168c8859a763c90b5b692be3e7bb7fe
WIZARD3022 <vinitsheetal15@gmail.com>
2023-12-27 18:30:17 +0530

    Code is color changing

---
+++ script.js
@@ -0,0 +1,189 @@
+/*
+var red = style.color="red";
+var green = style.color="green";
+var blue = style.color="blue";
+var aqua = style.color="aqua";
+var voilet = style.color="voilet";
+
+var redg = style.backgroundcolor="red";
+var greeng = style.background.color="green";
+var blueg = style.background.color="blue";
+var aquag = style.background.color="aqua";
+var voiletg = style.background.color="voilet";
+*/
+
+num1 = Math.floor(Math.random() * 5 + 1)
+num2 = Math.floor(Math.random() * 5 + 1)
+num3 = Math.floor(Math.random() * 5 + 1)
+num4 = Math.floor(Math.random() * 5 + 1)
+num5 = Math.floor(Math.random() * 5 + 1)
+num6 = Math.floor(Math.random() * 5 + 6)
+num7 = Math.floor(Math.random() * 5 + 6)
+num8 = Math.floor(Math.random() * 5 + 6)
+num9 = Math.floor(Math.random() * 5 + 6)
+num10 = Math.floor(Math.random() * 5 + 6)
+
+console.log(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10)
+
+if (num1 == 1) {
+    document.body.childNodes[1].childNodes[1].style.color = "red";
+}
+else if (num1 == 2) {
+    document.body.childNodes[1].childNodes[1].style.color = "green";
+}
+else if (num1 == 3) {
+    document.body.childNodes[1].childNodes[1].style.color = "blue";
+}
+else if (num1 == 4) {
+    document.body.childNodes[1].childNodes[1].style.color = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[1].style.color = "violet";
+}
+
+if (num2 == 1) {
+    document.body.childNodes[1].childNodes[3].style.color = "red";
+}
+else if (num2 == 2) {
+    document.body.childNodes[1].childNodes[3].style.color = "green";
+}
+else if (num2 == 3) {
+    document.body.childNodes[1].childNodes[3].style.color = "blue";
+}
+else if (num2 == 4) {
+    document.body.childNodes[1].childNodes[3].style.color = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[3].style.color = "violet";
+}
+
+if (num3 == 1) {
+    document.body.childNodes[1].childNodes[5].style.color = "red";
+}
+else if (num3 == 2) {
+    document.body.childNodes[1].childNodes[5].style.color = "green";
+}
+else if (num3 == 3) {
+    document.body.childNodes[1].childNodes[5].style.color = "blue";
+}
+else if (num3 == 4) {
+    document.body.childNodes[1].childNodes[5].style.color = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[5].style.color = "violet";
+}
+
+if (num4 == 1) {
+    document.body.childNodes[1].childNodes[7].style.color = "red";
+}
+else if (num4 == 2) {
+    document.body.childNodes[1].childNodes[7].style.color = "green";
+}
+else if (num4 == 3) {
+    document.body.childNodes[1].childNodes[7].style.color = "blue";
+}
+else if (num4 == 4) {
+    document.body.childNodes[1].childNodes[7].style.color = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[7].style.color = "violet";
+}
+
+if (num5 == 1) {
+    document.body.childNodes[1].childNodes[9].style.color = "red";
+}
+else if (num5 == 2) {
+    document.body.childNodes[1].childNodes[9].style.color = "green";
+}
+else if (num5 == 3) {
+    document.body.childNodes[1].childNodes[9].style.color = "blue";
+}
+else if (num5 == 4) {
+    document.body.childNodes[1].childNodes[9].style.color = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[9].style.color = "violet";
+}
+
+/*-----------------------------------------------------------------------------------------*/
+
+
+if (num6 == 6) {
+    document.body.childNodes[1].childNodes[1].style.background = "red";
+}
+else if (num6 == 7) {
+    document.body.childNodes[1].childNodes[1].style.background = "green";
+}
+else if (num6 == 8) {
+    document.body.childNodes[1].childNodes[1].style.background = "blue";
+}
+else if (num6 == 9) {
+    document.body.childNodes[1].childNodes[1].style.background = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[1].style.background = "violet";
+}
+
+if (num7 == 6) {
+    document.body.childNodes[1].childNodes[3].style.background = "red";
+}
+else if (num7 == 7) {
+    document.body.childNodes[1].childNodes[3].style.background = "green";
+}
+else if (num7 == 8) {
+    document.body.childNodes[1].childNodes[3].style.background = "blue";
+}
+else if (num7 == 9) {
+    document.body.childNodes[1].childNodes[3].style.background = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[3].style.background = "violet";
+}
+
+if (num8 == 6) {
+    document.body.childNodes[1].childNodes[5].style.background = "red";
+}
+else if (num8 == 7) {
+    document.body.childNodes[1].childNodes[5].style.background = "green";
+}
+else if (num8 == 8) {
+    document.body.childNodes[1].childNodes[5].style.background = "blue";
+}
+else if (num8 == 9) {
+    document.body.childNodes[1].childNodes[5].style.background = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[5].style.background = "violet";
+}
+
+if (num9 == 6) {
+    document.body.childNodes[1].childNodes[7].style.background = "red";
+}
+else if (num9 == 7) {
+    document.body.childNodes[1].childNodes[7].style.background = "green";
+}
+else if (num9 == 8) {
+    document.body.childNodes[1].childNodes[7].style.background = "blue";
+}
+else if (num9 == 9) {
+    document.body.childNodes[1].childNodes[7].style.background = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[7].style.background = "violet";
+}
+
+if (num10 == 6) {
+    document.body.childNodes[1].childNodes[9].style.background = "red";
+}
+else if (num10 == 7) {
+    document.body.childNodes[1].childNodes[9].style.background = "green";
+}
+else if (num10 == 8) {
+    document.body.childNodes[1].childNodes[9].style.background = "blue";
+}
+else if (num10 == 9) {
+    document.body.childNodes[1].childNodes[9].style.background = "aqua";
+}
+else {
+    document.body.childNodes[1].childNodes[9].style.background = "violet";
+}
